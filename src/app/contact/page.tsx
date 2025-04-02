509e4ca1e93c1143e9913f264f3ce5397013e9fa
Israt Jahan Naishe <naishe@Mac.home>
2025-04-02 21:36:53 +0300

    simple form is added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,29 @@
 import Header from "@/components/Header";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function ContactPage() {
     return (
         <div>
             <Header />
-            <h1>Contact us</h1>
+            <div className="max-w-md mx-auto mt-10 p-6 border rounded-lg shadow-md">
+                <h1 className="text-xl font-bold text-center mb-4">Contact Us</h1>
+                <form className="space-y-4">
+                    <div>
+                        <label className="block text-sm font-medium">Name</label>
+                        <Input placeholder="Your Name"/>
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium">Email</label>
+                        <Input type="email" placeholder="Your Email" />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium">Message</label>
+                        <Input placeholder="Your Message" />
+                    </div>
+                    <Button type="submit" className="w-full">Send Message</Button>
+                </form>
+            </div>
         </div>
     )
 }
