a22fbbd82b4a313d192fbafcd774305f2ef9e0f9
Israt Jahan Naishe <naishe@Mac.home>
2025-04-02 12:56:09 +0300

    changed style of the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,16 @@
+import Link from "next/link"
+
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
-      <h2 className="text-xl">My next.js project</h2>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="/" className="hover:text-blue-200">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="hover:text-blue-200">
-              About
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="bg-blue-600 text-white p-4">
+    <div className="container mx-auto flex justify-between">
+      <h1 className="text-xl font-bold">My next.js project</h1>
+      <div className="space-x-4">
+        <Link href="/" className="hover:underline">Home</Link>
+        <Link href="/about" className="hover:underline">About</Link>
+      </div>
+    </div>
+  </nav>
   )
 }
 export default Header
